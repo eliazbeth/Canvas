0594ef7daa55f44370b6fc083571bb7abcb053c1
Elizabeth Hernandez <elizabethhernandez.fl@gmail.com>
2024-01-20 12:23:38 -0500

    Create ContentItem class and add properties

---
+++ Canvas/models/ContentItem.cs
@@ -0,0 +1,13 @@
+namespace Canvas.Models
+{
+    public class ContentItem
+    {
+        public string? Name{get; set;}
+        public string? Description{get; set;}
+        public string? Path{get; set;}
+        ContentItem()
+        {
+            
+        }
+    }
+}
