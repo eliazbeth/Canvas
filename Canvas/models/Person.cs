be1806e17c88af639c1746f6437c0519ba480657
Elizabeth Hernandez <elizabethhernandez.fl@gmail.com>
2024-01-20 12:14:38 -0500

    Add properties to Person class

--- Canvas/models/Person.cs
+++ Canvas/models/Person.cs
@@ -1,7 +1,13 @@
-public class Person
-{   
-    public Person()
-    {
+namespace Canvas.Models
+{
+    public class Person
+    {   
+        public string? Name{get; set;}
+        public string? Classification{get; set;}
+        public string? Grades{get; set;}
+        public Person()
+        {
 
+        }
     }
 }
