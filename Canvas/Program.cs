3524275880a3ff9fa408c00e5e83e55d9c6ee2c4
Elizabeth Hernandez <elizabethhernandez.fl@gmail.com>
2024-01-21 13:46:55 -0500

    Implement AddCourse function For adding a course to a list of courses

--- Canvas/Program.cs
+++ Canvas/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Canvas.Models;
 
 namespace Canvas // Note: actual namespace depends on the project name.
 {
@@ -6,7 +7,28 @@ namespace Canvas // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            
+            List<Course> courses = new List<Course>();
+            AddCourse(courses);
+            foreach(Course c in courses)
+            {
+                Console.WriteLine(c);
+            }
+
         }
+        public static void AddCourse(List<Course> courses)
+        {
+                Console.WriteLine("Code:");
+                var code = Console.ReadLine();
+
+                Console.WriteLine("Name:");
+                var name = Console.ReadLine();
+
+                Console.WriteLine("Description:");
+                var description = Console.ReadLine();
+
+                Course myCourse = new Course{Code = code, Name = name, Description = description};
+                courses.Add(myCourse);
+        }
+
     }
 }
