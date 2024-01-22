f269fbab2c1e22cc661682674efaf1cf65b7b10d
Elizabeth Hernandez <elizabethhernandez.fl@gmail.com>
2024-01-22 11:58:09 -0500

    Implement ListCourses function

--- Canvas/Program.cs
+++ Canvas/Program.cs
@@ -7,47 +7,34 @@ namespace Canvas // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
+            // Create list of courses and add a course to it
             List<Course> courses = new List<Course>();
             AddCourse(courses);
-            courses.ForEach(Console.WriteLine);
-            PrintList(courses);
+            // Print list of courses
+            ListCourses(courses);
             Console.WriteLine();
             Course myCourse = courses[0];
 
+            // Create list of students and add a student to it
             List<Person> students = new List<Person>();
             AddStudent(students);
             AddStudent(students);
-            foreach(Person s in students)
-            {
-                Console.WriteLine(s);
-            }
+            // Print list of students
+            students.ForEach(Console.WriteLine);
             Console.WriteLine();
 
+            // Create course roster and add students to it
             myCourse.Roster = new List<Person>();
             AddStudentToCourse(students[0], myCourse);
             AddStudentToCourse(students[1], myCourse);
+            // Print course roster
             Console.WriteLine(myCourse + " roster:");
-            foreach(Person s in myCourse.Roster)
-            {
-                Console.WriteLine(s);
-            }
+            myCourse.Roster.ForEach(Console.WriteLine);
 
+            // Remove student from roster and print it again
             RemoveStudentFromCourse(students[0], myCourse);
             Console.WriteLine(myCourse + " roster:");
-            foreach(Person s in myCourse.Roster)
-            {
-                Console.WriteLine(s);
-            }
-
-
-
-        }
-        public static void PrintList<T>(List<T> theList)
-        {
-            foreach (T t in theList)
-            {
-                Console.WriteLine(t);
-            }
+            myCourse.Roster.ForEach(Console.WriteLine);
         }
         public static void AddCourse(List<Course> courses)
         {
@@ -81,6 +68,11 @@ namespace Canvas // Note: actual namespace depends on the project name.
         public static void RemoveStudentFromCourse(Person student, Course course)
         {
             course.Roster?.Remove(student);
+        }
+
+        public static void ListCourses(List<Course> courses)
+        {
+            courses.ForEach(Console.WriteLine);
         }
 
     }
