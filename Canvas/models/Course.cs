7d5fe08088ea65541db1933041f06e027172b99f
Elizabeth Hernandez <elizabethhernandez.fl@gmail.com>
2024-01-24 12:05:19 -0500

    Initialize list properties in constructor for Course class

--- Canvas/models/Course.cs
+++ Canvas/models/Course.cs
@@ -5,12 +5,14 @@ namespace Canvas.Models
           public string? Code{get; set;}
           public string? Name{get; set;}
           public string? Description{get; set;}
-          public List<Person>? Roster{get; set;}
-          public List<Assignment>? Assignments{get; set;}
-          public List<Module>? Modules{get; set;}
+          public List<Person> Roster{get; set;}
+          public List<Assignment> Assignments{get; set;}
+          public List<Module> Modules{get; set;}
           public Course()
           {
-
+                Roster = new List<Person>();
+                Assignments = new List<Assignment>();
+                Modules = new List<Module>();
           }
 
         public override string ToString()
