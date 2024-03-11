f3ea0d0a48f7ca5a7c5e67b0b638b737772e62e0
Elizabeth Hernandez <elizabethhernandez.fl@gmail.com>
2024-03-11 14:29:13 -0400

    Cleanup commented out template code

--- MAUI.Canvas/MainPage.xaml.cs
+++ MAUI.Canvas/MainPage.xaml.cs
@@ -2,25 +2,10 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
-
-	/*private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}*/
-
 	private void StudentViewClicked(object sender, EventArgs e)
 	{
 		Shell.Current.GoToAsync("//Students");
